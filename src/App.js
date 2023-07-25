4f67acca682d460f3bef070218af332ddea6866e
Ann Likowo Molinge <ann.molinge@gmail.com>
2023-07-25 01:16:39 -0500

    API call practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import { useState,useEffect } from 'react';
 import './App.css';
+import React from 'react';
+import {useParams} from "react-router-dom";
 
-function App() {
+function  App() {
+  // API key
+  const apiUrl = `https://jsonplaceholder.typicode.com/posts`
+  const params = useParams();
+ 
+  //Declare states to the data
+  const [data,setData] = useState([]);
+
+   const getData = async() =>{
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+    const arrayOfData = await response.json();
+    console.log(arrayOfData)
+    setData(arrayOfData)
+   }
+
+   // useEffect to run getCoin when component mounts
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Users </h1>
+{data.map((text) => <li>{text.body}/ {text.id}</li>)}
+
+
+
     </div>
   );
 }
